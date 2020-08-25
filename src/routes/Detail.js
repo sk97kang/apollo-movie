b70ee0e0638ed97ff2e450c5e5899f527c83faed
sk97kang <34053363+sk97kang@users.noreply.github.com>
2020-08-25 20:18:49 +0900

    #1.5 Suggestions & Optional Chaining

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -13,6 +13,10 @@ const GET_MOVIE = gql`
       language
       rating
     }
+    suggestions {
+      id
+      medium_cover_image
+    }
   }
 `;
 
@@ -28,6 +32,7 @@ const Container = styled.div`
 
 const Column = styled.div`
   margin-left: 10px;
+  width: 50%;
 `;
 
 const Title = styled.h1`
@@ -48,6 +53,9 @@ const Poster = styled.div`
   width: 25%;
   height: 60%;
   background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
 `;
 function Detail() {
   const { id } = useParams();
@@ -57,11 +65,17 @@ function Detail() {
   return (
     <Container>
       <Column>
-        <Title>Name</Title>
-        <Subtitle>English - 4.5</Subtitle>
-        <Description>lorem iew k wek ew</Description>
+        <Title>{loading ? "Loading..." : data.movie.title}</Title>
+        {!loading && data.movie && (
+          <>
+            <Subtitle>
+              {data.movie.language} - {data.movie.rating}
+            </Subtitle>
+            <Description>{data.movie.description_intro}</Description>
+          </>
+        )}
       </Column>
-      <Poster></Poster>
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
     </Container>
   );
 }
