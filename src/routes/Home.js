94a0b9c3ba70c5a0e19249ca6e27d47c9137d207
sk97kang <34053363+sk97kang@users.noreply.github.com>
2020-08-25 18:45:28 +0900

    #1.1 GET MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 function Home() {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => (
+      <img key={m.id} src={m.medium_cover_image} alt="poster" />
+    ));
+  }
   return <div>Home</div>;
 }
 
